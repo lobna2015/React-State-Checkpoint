231d7cbee43014876545a073a839543ad733a64c
LobnaDRISSI <lobnadrissi2020@gmail.com>
2022-02-16 20:14:31 +0100

    Second commit

--- src/App2.js
+++ src/App2.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 import Child1 from './Components/Child1';
 import Child2 from './Components/Child2';
-
+import './App.css';
+import './index.css';
 
 export default class App2 extends Component {
 
@@ -19,12 +20,14 @@ state = {
   render() {
     return (
       <div className="app2">
+<h1>List Of Student</h1>
+        <div className="container">
   
       { this.state.shows1 && <Child2/>}
       <button onClick={ ()=> this.setState({shows1 : !this.state.shows1 })}>toggleShowProfileLobna</button>
       { this.state.shows2 && <Child1/>}
       <button onClick={ ()=> this.setState({shows2 : !this.state.shows2 })}>toggleShowProfileSami</button>
- 
+      </div>
       </div>
     )
   }
